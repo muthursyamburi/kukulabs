972900840682b1133b757d7d46c68ba9692a0668
Dilip Muthukurussimana <muthursyamburi@gmail.com>
2014-04-06 23:17:44 -0500

    A small editor with just a multiline text control and nothing else!

--- pgms/python/frist.py
+++ pgms/python/frist.py
@@ -1,5 +1,13 @@
 import wx
+
+class MainWindow(wx.Frame):
+	def __init__(self, parent, id, title):
+		wx.Frame.__init__(self, parent, wx.ID_ANY, title, size =(450,200), style=wx.DEFAULT_FRAME_STYLE | wx.NO_FULL_REPAINT_ON_RESIZE )
+		self.control = wx.TextCtrl(self, style = wx.TE_MULTILINE )
+		self.Show(True)
+
 app = wx.App()
-window = wx.Frame(None, -1, "First Window!")
-window.Show()
+window = MainWindow(None, -1, "Small Editor")
+# window = wx.Frame(None, -1, "First Window!")
+# window.Show()
 app.MainLoop()
